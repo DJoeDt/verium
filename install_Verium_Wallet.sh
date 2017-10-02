e129d4bb18652885b5db692c2831b5dbd37d9428
DjoeDt <jochen@math.uni-bielefeld.de>
2017-10-02 21:30:19 +0000

    First colored version, untested

---
+++ install_Verium_Wallet.sh
@@ -0,0 +1,46 @@
+echo -e "\e[92mVerium Guide 1/10: \e[93mUpdate & Upgrade System\e[39m"
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -f -y
+echo -e "\e[92mVerium Guide 2/10: \e[93mInstall dependencies\e[39m"
+sudo apt-get install unzip build-essential libboost-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev libboost-all-dev libqrencode-dev freeglut3-dev git build-essential automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev -y
+echo -e "\e[92mVerium Guide 3/10: \e[93mCreating verium directory\e[39m"
+mkdir ~/verium
+echo -e "\e[92mVerium Guide 3/10: \e[93mClone wallet source code into ~/erium/wallet\e[39m"
+git clone https://github.com/VeriumReserve/verium ~/verium/wallet
+cd ~/verium/wallet/src
+echo -e "\e[92mVerium Guide 4/10: \e[93mCompile the wallet using make\e[39m"
+make -f makefile.unix
+echo -e "\e[92mVerium Guide 5/10: \e[93mWriting configuration file, edit by nano ~/verium/wallet/src/verium.conf\e[39m"
+echo "addnode=vrmsupernode.vericoin.info" > ~/verium/wallet/src/verium.conf
+echo "server=1" >> ~/verium/wallet/src/verium.conf
+echo "listen=1" >> ~/verium/wallet/src/verium.conf
+echo "daemon=1" >> ~/verium/wallet/src/verium.conf
+echo "rpcuser=YourSoloMiningName" >> ~/verium/wallet/src/verium.conf
+echo "rpcpassword=YourSoloMiningPassword" >> ~/verium/wallet/src/verium.conf
+echo "rpcallowip=localhost" >> ~/verium/wallet/src/verium.conf
+~/verium/wallet/src/veriumd
+echo -e "\e[92mVerium Guide 5/10: \e[93mGenerating Wallet. Wait 1 min\e[39m"
+sleep 60
+echo -e "\e[92mVerium Guide 6/10: \e[93mWallet generated, stopping it for now...\e[39m"
+~/verium/wallet/src/veriumd stop
+sleep 10
+echo -e "\e[92mVerium Guide 7/10: \e[93mDirectly download the Blockchain for speed up...\e[39m"
+wget -O ~/verium/bootstrap_VRM.zip http://www.vericoin.info/downloads/bootstrap_VRM.zip
+cd ~/verium
+echo -e "\e[92mVerium Guide 8/10: \e[93munzip and install the Blockchain...\e[39m"
+unzip bootstrap_VRM.zip
+rm -rf ~/.verium/txleveldb
+rm -rf ~/.verium/blk0001.dat
+mv ~/verium/bootstrap/blk0001.dat ~/.verium/blk0001.dat
+mv ~/verium/bootstrap/txleveldb ~/.verium/txleveldb
+sleep 5
+echo -e "\e[92mVerium Guide 9/10: \e[93mBlockchain update complete, restarting wallet\e[39m"
+~/verium/wallet/src/veriumd
+echo -e "\e[92mVerium Guide 10/10: \e[93mto get info use ~/verium/wallet/src/veriumd getinfo\e[39m" 
+sleep 10
+~/verium/wallet/src/veriumd getinfo
+sleep 5
+echo -e "\e[92mVerium Guide 10/10: \e[93mYour wallets address is:\e[39m"
+~/verium/wallet/src/veriumd getaddressesbyaccount ""
+echo -e "\e[92mVerium Guide 10/10: \e[93mCopy it for your miners to use\n"
+echo -e "\e[92mWelcome to Verium! \e[35m :)"
+echo -e "\e[39m"
