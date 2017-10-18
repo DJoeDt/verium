0dab72c56ac4a18f1a1d941b1025ab1b42c7362d
DJoeDt <Jochen@math.uni-bielefeld.de>
2017-10-18 18:48:29 +0200

    fix double sysfs, dir: 1wayminer

--- install_Verium_Miner_XU4.sh
+++ install_Verium_Miner_XU4.sh
@@ -4,16 +4,16 @@ echo -e "\e[92mVerium Guide 2/6: \e[93mInstall dependencies\e[39m"
 sudo apt-get install git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev -y
 echo -e "\e[92mVerium Guide 3/6: \e[93mCreating verium directory\e[39m"
 mkdir ~/verium
-echo -e "\e[92mVerium Guide 3/6: \e[93mClone miner source code into ~/verium/miner\e[39m"
-git clone https://github.com/effectsToCause/veriumMiner ~/verium/miner
+echo -e "\e[92mVerium Guide 3/6: \e[93mClone miner source code into ~/verium/1wayminer\e[39m"
+git clone https://github.com/effectsToCause/veriumMiner ~/verium/1wayminer
 echo -e "\e[92mVerium Guide 4/6: \e[93mConfigure Algorithm\e[39m"
-sed -i -e 's/#define SCRYPT_MAX_WAYS 3/#define SCRYPT_MAX_WAYS 1/g' ~/verium/miner/algo/scrypt.c
-sed -i -e 's/#define HAVE_SCRYPT_3WAY 1/\/\/#define HAVE_SCRYPT_3WAY 1/g' ~/verium/miner/algo/scrypt.c
-sed -i -e 's/#define scrypt_best_throughput() 3/#define scrypt_best_throughput() 1/g' ~/verium/miner/algo/scrypt.c
-sed -i -e 's/void scrypt_core_3way/void scrypt_core /g' ~/verium/miner/algo/scrypt.c
-sed -i -e 's/-DUSE_ASM -pg/-DUSE_ASM -mfpu=neon -pg/g' ~/verium/miner/build.sh
+sed -i -e 's/#define SCRYPT_MAX_WAYS 3/#define SCRYPT_MAX_WAYS 1/g' ~/verium/1wayminer/algo/scrypt.c
+sed -i -e 's/#define HAVE_SCRYPT_3WAY 1/\/\/#define HAVE_SCRYPT_3WAY 1/g' ~/verium/1wayminer/algo/scrypt.c
+sed -i -e 's/#define scrypt_best_throughput() 3/#define scrypt_best_throughput() 1/g' ~/verium/1wayminer/algo/scrypt.c
+sed -i -e 's/void scrypt_core_3way/void scrypt_core /g' ~/verium/1wayminer/algo/scrypt.c
+sed -i -e 's/-DUSE_ASM -pg/-DUSE_ASM -mfpu=neon -pg/g' ~/verium/1wayminer/build.sh
 echo -e "\e[92mVerium Guide 4/6: \e[93mCompile the miner using build\e[39m"
-cd verium/miner
+cd verium/1wayminer
 ./build.sh
 echo -e "\e[92mVerium Guide 5/6: \e[93mMiner built.\e[39m"
 echo -e "\e[92mVerium Guide 6/6: \e[93mTesting the miner with a little joke... ;) \e[39m"
@@ -47,7 +47,7 @@ sudo echo 'MAX_SPEED=1900000' >> /etc/default/cpufrequtils
 sudo echo 'MIN_SPEED=1900000' >> /etc/default/cpufrequtils
 echo -e "\e[92mXU4 Guide 2/ : \e[93m Downclocking the XU4 GPU"
 echo "$: sudo apt-get install sysfsutils"
-sudo apt-get install sysfsutilssudo apt-get install sysfsutils
+sudo apt-get install sysfsutils
 echo -e "\e[92mXU4 Guide 2/ : \e[93mConfigure sysfs.conf to powersave"
 echo "# Put GPU into powersave mode (= Downclocking it)"  >>  /etc/sysfs.conf
 echo "devices/platform/11800000.mali\:/devfreq/11800000.mali\:/governor = powersave" >>  /etc/sysfs.conf
@@ -56,5 +56,4 @@ echo "$: service sysfsutils start"
 service sysfsutils start
 /media/boot/boot.ini
 # TODO: add setenv ddr_freq 933
-echo "edit ~/tmpmnt/boot.ini"
 echo -e "\e[39m"
