f2e79e6a32881d4f171972ccd6ef037a06e2518d
DJoeDt <Jochen@math.uni-bielefeld.de>
2018-01-27 12:16:53 +0100

    fixes

--- 1wayARM_fireworm_install.sh
+++ 1wayARM_fireworm_install.sh
@@ -1,20 +1,15 @@
-echo -e "\e[92mVerium Guide 1/6: \e[93mUpdate & Upgrade System\e[39m"
+echo "\e[92mVerium Guide 1/6: \e[93mUpdate & Upgrade System\e[39m"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -f -y
 echo -e "\e[92mVerium Guide 2/6: \e[93mInstall dependencies\e[39m"
 sudo apt-get install git automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev -y
 echo -e "\e[92mVerium Guide 3/6: \e[93mCreating verium directory\e[39m"
 mkdir ~/verium
-echo -e "\e[92mVerium Guide 3/6: \e[93mClone miner source code into ~/verium/miner\e[39m"
+echo -e "\e[92mVerium Guide 3/6: \e[93mClone miner source code into ~/verium/fireworm\e[39m"
 git clone https://github.com/fireworm71/veriumMiner.git ~/verium/fireworm
 echo -e "\e[92mVerium Guide 4/6: \e[93mCompile the miner using build\e[39m"
 cd verium/fireworm
 ./build.sh
 echo -e "\e[92mVerium Guide 5/6: \e[93mMiner built.\e[39m"
-sed -i -e 's/exit 0//g' /etc/rc.local
-echo "/home/pi/verium/1wayminer/cpuminer -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u joe_rondx.RPi3 -p joe -1 4 --api-bind 0.0.0.0:4048 --no-color >> /home/pi/verium/fireworm/mine.log &" >> /etc/rc.local
-echo "" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
-echo "" >> /etc/rc.local
 echo "/opt/vc/bin/vcgencmd measure_temp >> ~/verium/cpu_temp.log" > ~/verium/templog.sh
 chmod +x ~/verium/templog.sh
 echo "*/10 * *   *   *  /home/pi/verium/templog.sh"
@@ -36,6 +31,10 @@ sdram_schmoo=0x02000020
 over_voltage_sdram_p=6
 over_voltage_sdram_i=4
 over_voltage_sdram_c=4"
+echo "Autostart config:"
+echo "sudo nano /etc/rc.local"
+echo "/home/pi/verium/fireworm/cpuminer -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u joe_rondx.RPi3 -p joe -1 4 --api-bind 0.0.0.0:4048 --no-color >> /home/pi/verium/fireworm/mine.log &" >> /etc/rc.local
+echo "before exit 0"
 echo -e "\e[92mHappy Verium Mining! \e[35m joe_rondx :)"
 echo -e "      \e[94m_\e[34m_"
 echo -e "  \e[37m_  \e[94m/  \e[34m\  \e[90m_"
