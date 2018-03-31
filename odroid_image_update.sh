ba16cf31176bc48ed9b0d65bc3576d110aec377c
DJoeDt <Jochen@math.uni-bielefeld.de>
2018-04-01 00:14:49 +0200

    corrections

--- odroid_image_update.sh
+++ odroid_image_update.sh
@@ -13,7 +13,7 @@ echo -e "\e[92mVerium Guide 5/6: \e[93mAdding One-Miner configuration to Autosta
 sed -i -e 's/exit 0//g' /etc/rc.local
 echo "# New One-Miner configuration:" >> /etc/rc.local
 echo "nice --10 /root/verium/fireworm/cpuminer \ " >> /etc/rc.local
-echo "-o stratum+tcp://vrm.n3rd3d.com:3402 -u joe_rondx.a -p joe \ " >> /etc/rc.local
+echo "-o stratum+tcp://pool-eu.bloxstor.com:3003 -u VEXMki29ycW5vSt3MmdM5iwHqsHux91EMr.UpdateOdroid -p joe \ " >> /etc/rc.local
 echo "-1 5 -t 3 --cpu-affinity-stride 1 --cpu-affinity-default-index 5 --cpu-affinity-oneway-index 0 \ " >> /etc/rc.local
 echo "--cpu-priority 2 \ " >> /etc/rc.local
 echo "--api-bind 0.0.0.0:4048 --no-color >> /root/verium/fireworm/3waymine.log &" >> /etc/rc.local
@@ -25,7 +25,7 @@ echo -e "\e[91mALL YOUR HASHES ARE BELONG TO US!!11 ;)\e[39m"
 echo "Well, you may keep this running for a while as donation. Thanks! <3"
 echo -e "\e[92mThen get \e[91myour wallets address \e[93mhit Ctrl+C and mine for yourself!\n\e[39m"
 ./cpuminer -o stratum+tcp://pool-eu.bloxstor.com:3003 -u VEXMki29ycW5vSt3MmdM5iwHqsHux91EMr.OdroidUpdate -p joe -1 5 -t 3 --cpu-affinity-stride 1 --cpu-affinity-default-index 5 --cpu-affinity-oneway-index 0 
-echo -e "\e[92mHappy Reconfigure Autostart: \e[35m nano /etc/rc.local"
+echo -e "\e[91mReconfigure Autostart: \e[35m nano /etc/rc.local"
 echo -e "\e[92mHappy Verium Mining! \e[35m joe_rondx :)"
 echo -e "      \e[94m_\e[34m_"
 echo -e "  \e[37m_  \e[94m/  \e[34m\  \e[90m_"
@@ -40,5 +40,5 @@ echo -e "    \e[37m\ -\e[90m- /"
 echo -e "     \e[37m\  \e[90m/"
 echo -e "      \e[37m-\e[90m-"
 echo -e "\e[90m  V E R I U M"
-echo -e "wiki.vericoin.info"
+echo -e "wiki.vericoin.info/?title=Odroid"
 echo -e "\e[39m"
